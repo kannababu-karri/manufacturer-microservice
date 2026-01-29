316754e79c6202bcef51bacae6d064293a409134
Kannababu.Karri <kkarri@gmail.com>
2026-01-29 12:28:08 -0500

    Added JWT authentication and authorization

--- src/main/java/com/restful/manufacturer/controller/ManufacturerController.java
+++ src/main/java/com/restful/manufacturer/controller/ManufacturerController.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.restful.manufacturer.entity.Manufacturer;
 import com.restful.manufacturer.exception.InvalidManufacturerException;
@@ -23,7 +23,7 @@ import com.restful.manufacturer.exception.ManufacturerNotFoundException;
 import com.restful.manufacturer.service.ManufacturerService;
 
 @CrossOrigin(origins = "http://localhost:4200")
-@Controller
+@RestController
 @RequestMapping("/api/manufacturer")
 public class ManufacturerController {
 	
@@ -37,6 +37,10 @@ public class ManufacturerController {
     }
     
     @PostMapping
+    	(
+    			consumes = MediaType.APPLICATION_JSON_VALUE,
+    			produces  = MediaType.APPLICATION_JSON_VALUE
+    	)
     public ResponseEntity<Manufacturer> createManufacturer(@RequestBody Manufacturer manufacturer) {
     	_LOGGER.info(">>> Inside createManufacturer. <<<");
         if (manufacturer.getMfgName() == null || manufacturer.getMfgName().isBlank()) {
@@ -45,7 +49,9 @@ public class ManufacturerController {
 
         Manufacturer saved = manufacturerService.saveOrUpdate(manufacturer);
         
-        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+        //return new ResponseEntity<>(saved, HttpStatus.CREATED);
+        //Using builder for 201 Created
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
     
     @DeleteMapping("/{id}")
@@ -120,7 +126,8 @@ public class ManufacturerController {
             //throw new ManufacturerNotFoundException("No manufacturers found for manufacturerNameLike: "+manufacturerNameLike);
         }
 
-        return ResponseEntity.ok(manufacturers);
+        //return ResponseEntity.ok(manufacturers);
+        return new ResponseEntity<>(manufacturers, HttpStatus.OK);
     }
 	
     @GetMapping("/name/{manufacturerName}")
